62d3f0d1007fb975fb48d73801db3edb3170ff61
ruzibekov <ruzibekov44@gmail.com>
2022-03-26 16:07:43 +0500

    all add

--- app/src/main/java/com/ruzibekov/todolist_uz/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/ruzibekov/todolist_uz/presentation/activity/MainActivity.kt
@@ -3,12 +3,16 @@ package com.ruzibekov.todolist_uz.presentation.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ruzibekov.todolist_uz.R
+import com.ruzibekov.todolist_uz.presentation.fragments.FocusModeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Test
+        supportFragmentManager.beginTransaction().replace(R.id.mainFragmentContainer, FocusModeFragment()).commit()
     }
 
 }
---
+++ app/src/main/java/com/ruzibekov/todolist_uz/presentation/fragments/FocusModeFragment.kt
@@ -0,0 +1,15 @@
+package com.ruzibekov.todolist_uz.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ruzibekov.todolist_uz.R
+
+class FocusModeFragment : Fragment(R.layout.fragment_focus_mode) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
