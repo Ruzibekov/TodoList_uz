6a67453081d4264cf0d7c1bc0c7b38fcb70d1a80
Mansurjon0608 <js1235gugl@gmail.com>
2022-03-30 14:32:45 +0500

    ProfileFragment

--- app/src/main/java/com/ruzibekov/todolist_uz/presentation/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/ruzibekov/todolist_uz/presentation/fragments/UserProfileFragment.kt
@@ -1,21 +1,81 @@
 package com.ruzibekov.todolist_uz.presentation.fragments
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.ruzibekov.todolist_uz.R
+import com.ruzibekov.todolist_uz.databinding.FragmentUserProfileBinding
 
 
 class UserProfileFragment : Fragment() {
 
+    lateinit var binding: FragmentUserProfileBinding
+    private lateinit var dialogChangeName: Dialog
+    private lateinit var dialogChangePassword: Dialog
+
+    @SuppressLint("InflateParams")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        binding = FragmentUserProfileBinding.inflate(layoutInflater)
+
+        binding.tvAppSetting.setOnClickListener {
+
+        }
+
+        binding.tvChangeName.setOnClickListener {
+
+            dialogChangeName = Dialog(requireContext(), androidx.appcompat.R.style.Theme_AppCompat_Light)
+            dialogChangeName.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialogChangeName.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialogChangeName.setContentView(R.layout.dialog_change_name)
+            dialogChangeName.setCanceledOnTouchOutside(false)
+            dialogChangeName.setCancelable(false)
+
+
+
+            dialogChangeName.show()
+        }
+
+        binding.tvChangePassword.setOnClickListener {
+
+            dialogChangePassword = Dialog(requireContext(), androidx.appcompat.R.style.Theme_AppCompat_Light)
+            dialogChangePassword.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialogChangePassword.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialogChangePassword.setContentView(R.layout.dialog_change_password)
+            dialogChangePassword.setCanceledOnTouchOutside(false)
+            dialogChangePassword.setCancelable(false)
+
+
+
+            dialogChangeName.show()
+        }
+
+
+
+        binding.tvChangeImage.setOnClickListener {
+            val bottumSheet = BottomSheetDialog(requireContext())
+            bottumSheet.setContentView(
+                layoutInflater.inflate(
+                    R.layout.dialog_bottom_change_image,
+                    null,
+                    false
+                )
+            )
+
+            bottumSheet.show()
+        }
+
+        return binding.root
     }
 
 
